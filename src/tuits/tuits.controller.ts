da1e13fd08a7b00c327224718c5c1ab26067ed95
MartynLCD3 <martincastagno63@gmail.com>
2022-03-10 14:09:45 -0300

    Configuracion @HttpCode, @Body, @Param - APIS: GET, PATCH, DELETE

--- src/tuits/tuits.controller.ts
+++ src/tuits/tuits.controller.ts
@@ -1,19 +1,42 @@
-import { 
+import {
 	Controller, 
 	Get,
-	Param
+	Patch,
+	Delete,
+	Post,
+	Param,
+	Body,
+	HttpCode,
+	Query
 } from '@nestjs/common';
 
 @Controller('tuits')
 export class TuitsController {
 
 	@Get() 
-	getTuits(): string {
-		return 'Hello from Tuitter';	
+	getTuits(@Query() filterQuery): string {
+		const { searchTerm, orderBy } = filterQuery; 
+		return `All ${searchTerm} tuits ordered by ${orderBy}`;	
 	}
 
 	@Get(':id')
 	getTuit(@Param('id') id: string ): string {
 		return `Your tuit id is ${id}`;
+	}
+
+	@Post()
+	@HttpCode(204)
+	createTuiter(@Body('message') body: string): string {
+		return `${body}`;
+	}
+	
+	@Patch(':id')
+	updateTuit(@Param('id') id: string, @Body() tuit): string {
+		return `The tuit ${id} has been updated`;
+	}
+
+	@Delete(':id')
+	deleteTuit(@Param('id') id: string): string {
+		return `The tuit ${id} has been deleted`;	
 	}
 }
